76ce09b4c71c6cb36f18c032d9d0942b706f8fb5
Matthew Earl <git@matthewearl.com>
2024-01-06 11:40:16 +0000

    hide cursor when freefly moving

--- trunk/gl_screen.c
+++ trunk/gl_screen.c
@@ -970,7 +970,7 @@ static void SCR_DrawCursor(void)
 	cursor_y = scr_pointer_state.y;
 
 	// Disable the cursor in all but following client parts
-	if (!CL_DemoUIOpen() && key_dest != key_menu)
+	if (FreeFly_Moving() || (!CL_DemoUIOpen() && key_dest != key_menu))
 		return;
 
 	// Always draw the cursor.
