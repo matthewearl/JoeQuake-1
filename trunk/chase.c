29dfd6a01db410497c0df1def5e872c000464235
Matthew Earl <git@matthewearl.com>
2025-02-09 14:05:35 +0000

    make third person look at player origin

--- trunk/chase.c
+++ trunk/chase.c
@@ -48,7 +48,7 @@ void TraceLine (vec3_t start, vec3_t end, vec3_t impact)
 	trace_t	trace;
 
 	memset (&trace, 0, sizeof(trace));
-	SV_RecursiveHullCheck (cl.worldmodel->hulls, 0, 0, 1, start, end, &trace);
+	SV_RecursiveHullCheck (&cl.worldmodel->hulls[1], 0, 0, 1, start, end, &trace);
 	VectorCopy (trace.endpos, impact);
 }
 
@@ -56,26 +56,19 @@ void Chase_Update (void)
 {
 	int	i;
 	float	dist;
-	vec3_t	forward, up, right, dest, stop;
+	vec3_t	forward, up, right, dest, start, stop;
 
 	// if can't see player, reset
 	AngleVectors (cl.viewangles, forward, right, up);
 
 	// calc exact destination
+	VectorCopy(r_refdef.vieworg, start);
+	start[2] -= cl.viewheight;
 	for (i=0 ; i<3 ; i++)
-		chase_dest[i] = r_refdef.vieworg[i] - forward[i]*cl_thirdperson_back.value - right[i]*cl_thirdperson_right.value;
-	chase_dest[2] = r_refdef.vieworg[2] + cl_thirdperson_up.value;
+		chase_dest[i] = start[i] - forward[i]*cl_thirdperson_back.value - right[i]*cl_thirdperson_right.value;
+	chase_dest[2] += cl_thirdperson_up.value;
 
-	// find the spot the player is looking at
-	VectorMA (r_refdef.vieworg, 4096, forward, dest);
-	TraceLine (r_refdef.vieworg, dest, stop);
-
-	// calculate pitch to look at the same spot from camera
-	VectorSubtract (stop, r_refdef.vieworg, stop);
-	dist = max(1, DotProduct(stop, forward));
-	r_refdef.viewangles[PITCH] = -atan(stop[2] / dist) / M_PI * 180;
-
-	TraceLine (r_refdef.vieworg, chase_dest, stop);
+	TraceLine (start, chase_dest, stop);
 	if (stop[0] != 0 || stop[1] != 0 || stop[2] != 0)
 		VectorCopy (stop, chase_dest);
 
